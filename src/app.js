a4d1b8d12da61cab88b84ef291fa97693e25fdae
“prashant-vw” <prashant.handel@visionwaves.com>
2025-07-09 01:22:28 +0530

    add: user router

--- src/app.js
+++ src/app.js
@@ -4,15 +4,22 @@ const connectDB = require('./config/database.js');
 const authRouter = require('./routes/auth.js');
 const cookieParser = require('cookie-parser');
 const taskRouter = require('./routes/task.js');
+const cors = require('cors');
+const userRouter = require('./routes/user.js');
 
 PORT = process.env.PORT;
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:4500',
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use('/auth', authRouter);
 app.use('/task', taskRouter);
+app.use('/user', userRouter);
 
 connectDB().then(() => {
     app.listen(8080, () => {
---
+++ src/routes/user.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const User = require('../models/user.js');
+const router = express.Router();
+const userAuth = require('../middlewares/auth.js');
+const getUserIdFromToken = require('../utils/getUserIdFromToken.js');
+
+router.get('/allUsers', userAuth, async (req, res) => {
+    try {
+        const userId = getUserIdFromToken(req);
+        const users = await User.find({
+            _id: { $ne: userId }
+        });
+        
+        if (!users?.length) {
+            return res.status(404).json({
+                status: false,
+                message: 'No users found'
+            });
+        }
+
+        res.json({
+            status: true,
+            message: 'Users fetched successfully',
+            users
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: 'Error fetching users',
+            error: error.message
+        })
+    }
+});
+
+router.get('/searchExpectSelf', userAuth, async (req, res) => {
+    try {
+        const { query } = req.query;
+
+        if (!query || query.trim() === '') {
+            return res.status(400).json({ message: 'Query parameter is required' });
+        }
+
+        const regex = new RegExp(query, 'i');
+        const userId = getUserIdFromToken(req);
+
+        const users = await User.find({
+            _id: { $ne: userId },
+            $or: [
+                { firstName: { $regex: regex } },
+                { middleName: { $regex: regex } },
+                { lastName: { $regex: regex } },
+                { email: { $regex: regex } }
+            ]
+        });
+        console.log(users);
+
+        if (!users?.length) {
+            return res.status(404).json({
+                status: false,
+                message: 'No users found'
+            });
+        }
+
+        res.json({
+            status: true,
+            message: 'Users fetched successfully',
+            users
+        })
+    } catch (error) {
+        res.status(500).json({
+            status: false,
+            message: 'Error searching users',
+            error: error.message
+        })
+    }
+})
+
+
+module.exports = router;

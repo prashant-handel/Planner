4b8e3b3aca014b579c5dccaa3bb66e566150170d
“prashant-vw” <prashant.handel@visionwaves.com>
2025-07-18 01:23:01 +0530

    fix: cors url

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ PORT = process.env.PORT;
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:4500', 'https://planner-client-one.vercel.app'],
+  origin: ['http://localhost:4500', 'https://planner-lgmo.onrender.com'],
   credentials: true,
 }));
 app.use(express.json());
